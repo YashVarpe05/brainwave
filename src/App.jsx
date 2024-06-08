d525959187b13044988775bb8ac55e4c9e5f8889
Yash_Varpe <yashvarpe2005@gmail.com>
2024-06-09 00:08:04 +0530

    add header section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
